2c826364970a6962f3be9137b4b6215a70789a58
Alana Gilston <abluescarab@users.noreply.github.com>
2020-01-26 04:15:03 -0800

    Moved tab group implementation to UITabGroup

--- UI/FBAUIState.cs
+++ UI/FBAUIState.cs
@@ -1,7 +1,5 @@
 ﻿using FullBodyAccessories.Categories;
-using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.GameContent.UI.Elements;
 using Terraria.Localization;
 using Terraria.UI;
 
@@ -10,13 +8,9 @@ namespace FullBodyAccessories.UI
     public class FBAUIState : UIState
     {
         private const int SlotMargin = 6;
-        private const int TextMargin = 15;
-
-        private int _lastPage = -1;
-        private int _currentPage = 0;
 
         public DraggableUIPanel Panel { get; private set; }
-        public UIText[] TextButtons { get; private set; }
+        public UITabGroup Tabs { get; private set; }
 
         public CategorizedSlotGroup BackGroup { get; private set; }
         public CategorizedSlotGroup HeadGroup { get; private set; }
@@ -37,58 +31,21 @@ namespace FullBodyAccessories.UI
             int y = 0;
             int slotSize = Main.inventoryBackTexture.Width;
             int slotOffset = slotSize + SlotMargin;
-            string equip = Language.GetTextValue("Equip");
-            string social = Language.GetTextValue("Social");
-            Vector2 equipSize = Main.fontMouseText.MeasureString(equip);
-
-            void onMouseOver(UIMouseEvent evt, UIElement element)
-            {
-                if (element is UIText text && text.TextColor != Color.White)
-                {
-                    text.TextColor = Color.LightGray;
-                }
-            }
-
-            void onMouseOut(UIMouseEvent evt, UIElement element)
-            {
-                if (element is UIText text && text.TextColor != Color.White)
-                {
-                    text.TextColor = Color.Gray;
-                }
-            }
 
             Panel = new DraggableUIPanel();
 
-            y = (int)equipSize.Y;
-
             // create the buttons to change equipment type
-            TextButtons = new UIText[3];
-
-            TextButtons[0] = new UIText(equip);
-            TextButtons[0].OnClick += (evt, element) => _currentPage = 0;
-            TextButtons[0].OnMouseOver += onMouseOver;
-            TextButtons[0].OnMouseOut += onMouseOut;
-            Panel.Append(TextButtons[0]);
-
-            TextButtons[1] = new UIText(social)
+            Tabs = new UITabGroup(new[]
             {
-                TextColor = Color.Gray
-            };
-            TextButtons[1].Left.Set(equipSize.X + TextMargin, 0);
-            TextButtons[1].OnClick += (evt, element) => _currentPage = 1;
-            TextButtons[1].OnMouseOver += onMouseOver;
-            TextButtons[1].OnMouseOut += onMouseOut;
-            Panel.Append(TextButtons[1]);
-
-            TextButtons[2] = new UIText(Language.GetTextValue("Dye"))
+                Language.GetTextValue("Equip"), Language.GetTextValue("Social"), Language.GetTextValue("Dye")
+            });
+            Tabs.OnSelectedIndexChanged += (evt, lastTab, currentTab) =>
             {
-                TextColor = Color.Gray
+                AppendAllCategories(currentTab);
             };
-            TextButtons[2].Left.Set(TextButtons[1].Left.Pixels + Main.fontMouseText.MeasureString(social).X + TextMargin, 0);
-            TextButtons[2].OnClick += (evt, element) => _currentPage = 2;
-            TextButtons[2].OnMouseOver += onMouseOver;
-            TextButtons[2].OnMouseOut += onMouseOut;
-            Panel.Append(TextButtons[2]);
+            Panel.Append(Tabs);
+
+            y = (int)Tabs.Height.Pixels;
 
             // create the equipment slot groups
             // TODO: replace new Category() calls with more accessible variables
@@ -116,45 +73,35 @@ namespace FullBodyAccessories.UI
             Panel.Left.Set(500, 0);
             Panel.Top.Set(300, 0);
             Panel.Width.Set(slotOffset * 3 + Panel.PaddingLeft + Panel.PaddingRight, 0);
-            Panel.Height.Set(slotOffset * 4 + equipSize.Y + Panel.PaddingTop + Panel.PaddingBottom, 0);
+            Panel.Height.Set(slotOffset * 4 + Tabs.Height.Pixels /*equipSize.Y*/ + Panel.PaddingTop + Panel.PaddingBottom, 0);
 
             Append(Panel);
+            AppendAllCategories(0);
         }
 
-        public override void Update(GameTime gameTime)
+        private void AppendAllCategories(int tab)
         {
-            base.Update(gameTime);
-
-            if (_currentPage != _lastPage)
-            {
-                AppendToPanel(BackGroup, HeadGroup, LeftArmGroup, NeckGroup, RightArmGroup, LeftRingGroup, WaistGroup,
-                       RightRingGroup, LeftFootGroup, RightFootGroup);
-
-                if (_lastPage > -1)
-                    TextButtons[_lastPage].TextColor = Color.Gray;
-
-                TextButtons[_currentPage].TextColor = Color.White;
-                _lastPage = _currentPage;
-            }
+            AppendToPanel(tab, BackGroup, HeadGroup, LeftArmGroup, NeckGroup, RightArmGroup, LeftRingGroup,
+                          WaistGroup, RightRingGroup, LeftFootGroup, RightFootGroup);
         }
 
-        private void AppendToPanel(params CategorizedSlotGroup[] groups)
+        private void AppendToPanel(int tab, params CategorizedSlotGroup[] groups)
         {
             foreach (CategorizedSlotGroup group in groups)
             {
-                if (_currentPage == 0)
+                if (tab == 0)
                 {
                     Panel.Append(group.Slot);
                     Panel.RemoveChild(group.SocialSlot);
                     Panel.RemoveChild(group.DyeSlot);
                 }
-                else if (_currentPage == 1)
+                else if (tab == 1)
                 {
                     Panel.RemoveChild(group.Slot);
                     Panel.Append(group.SocialSlot);
                     Panel.RemoveChild(group.DyeSlot);
                 }
-                else if (_currentPage == 2)
+                else if (tab == 2)
                 {
                     Panel.RemoveChild(group.Slot);
                     Panel.RemoveChild(group.SocialSlot);
---
+++ UI/UITabGroup.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ID;
+using Terraria.UI;
+
+namespace FullBodyAccessories.UI
+{
+    public class UITabGroup : UIElement
+    {
+        public delegate void SelectedTabChangedEvent(UIEvent evt, UIText lastTab, UIText currentTab);
+
+        public delegate void SelectedIndexChangedEvent(UIEvent evt, int lastTab, int currentTab);
+
+        public event SelectedIndexChangedEvent OnSelectedIndexChanged;
+        public event SelectedTabChangedEvent OnSelectedTabChanged;
+
+        private UIText _selectedTab = null;
+
+        public Color SelectedColor { get; set; }
+        public Color DeselectedColor { get; set; }
+        public Color SelectedHoverColor { get; set; }
+        public Color DeselectedHoverColor { get; set; }
+        public UIText[] Tabs { get; }
+        public int TabMargin { get; set; }
+
+        public UIText SelectedTab
+        {
+            get => _selectedTab;
+            set
+            {
+                if (Array.Find(Tabs, tab => tab == value) != null)
+                {
+                    ChangeSelectedTab(value);
+                }
+            }
+        }
+
+        public int SelectedIndex
+        {
+            get => Array.IndexOf(Tabs, SelectedTab);
+            set
+            {
+                if (value > -1 && value < Tabs.Length)
+                {
+                    ChangeSelectedTab(Tabs[value]);
+                }
+            }
+        }
+
+        public UITabGroup(string[] tabNames) : this(tabNames.ToList()) { }
+
+        public UITabGroup(List<string> tabNames)
+        {
+            Tabs = new UIText[tabNames.Count];
+            SelectedColor = Color.White;
+            DeselectedColor = Color.Gray;
+            SelectedHoverColor = Color.LightGray;
+            DeselectedHoverColor = SelectedHoverColor;
+            TabMargin = 10;
+
+            InitializeTabs(tabNames, out float width, out float height);
+
+            SelectedIndex = 0;
+            Width.Set(width, 0);
+            Height.Set(height, 0);
+        }
+
+        private void InitializeTabs(List<string> tabNames, out float width, out float height)
+        {
+            Vector2 firstTabSize = Main.fontMouseText.MeasureString(tabNames[0]);
+            float tabLeft = firstTabSize.X;
+
+            for (int i = 0; i < Tabs.Length; i++)
+            {
+                UIText tab = new UIText(tabNames[i])
+                {
+                    TextColor = DeselectedColor
+                };
+
+                tab.OnClick += (evt, element) =>
+                {
+                    SelectedTab = tab;
+                    Main.PlaySound(SoundID.MenuTick);
+                };
+
+                tab.OnMouseOver += (evt, element) =>
+                {
+                    tab.TextColor = SelectedTab == tab ? SelectedHoverColor : DeselectedHoverColor;
+                };
+
+                tab.OnMouseOut += (evt, element) =>
+                {
+                    tab.TextColor = SelectedTab == tab ? SelectedColor : DeselectedColor;
+                };
+
+                if (i > 0)
+                {
+                    tab.Left.Set(tabLeft + TabMargin, 0);
+                    tabLeft = tab.Left.Pixels + Main.fontMouseText.MeasureString(tab.Text).X;
+                }
+
+                Append(tab);
+                Tabs[i] = tab;
+            }
+
+            width = tabLeft;
+            height = firstTabSize.Y;
+        }
+
+        public virtual void SelectedTabChanged(UIEvent evt, UIText lastTab, UIText currentTab)
+        {
+            OnSelectedTabChanged?.Invoke(evt, lastTab, currentTab);
+        }
+
+        public virtual void SelectedIndexChanged(UIEvent evt, int lastTab, int currentTab)
+        {
+            OnSelectedIndexChanged?.Invoke(evt, lastTab, currentTab);
+        }
+
+        private void ChangeSelectedTab(UIText newTab)
+        {
+            int tabIndex = SelectedIndex;
+            UIText tab = SelectedTab;
+
+            if (_selectedTab != null)
+                _selectedTab.TextColor = DeselectedColor;
+
+            _selectedTab = newTab;
+            _selectedTab.TextColor = SelectedColor;
+
+            UIEvent evt = new UIEvent(this);
+
+            SelectedTabChanged(evt, tab, SelectedTab);
+            SelectedIndexChanged(evt, tabIndex, SelectedIndex);
+        }
+    }
+}
