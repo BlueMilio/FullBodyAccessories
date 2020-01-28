caedc021a4e64fa041ace57082aae32f849f0769
Alana Gilston <abluescarab@users.noreply.github.com>
2020-01-28 05:14:03 -0800

    Added config file to control CustomUIPanel visibility

---
+++ FBAConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using FullBodyAccessories.UI;
+using Terraria.ModLoader.Config;
+
+namespace FullBodyAccessories
+{
+    public class FBAConfig : ModConfig
+    {
+        public static FBAConfig Instance;
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [DefaultValue(false)]
+        [Label("Hide draggable panel")]
+        public bool HidePanel;
+
+        public override void OnChanged()
+        {
+            FBAUIState ui = ((FBAMod)mod).SlotUI;
+
+            if (ui == null) return;
+
+            ((FBAMod)mod).SlotUI.Panel.Visible = !HidePanel;
+        }
+    }
+}
