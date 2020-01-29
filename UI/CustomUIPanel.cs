aa257d8e4b6894e052d2ef7ab2679c02d8477089
Alana Gilston <abluescarab@users.noreply.github.com>
2020-01-29 03:56:25 -0800

    Fixed braces, allows dragging from any edge

--- UI/CustomUIPanel.cs
+++ UI/CustomUIPanel.cs
@@ -4,8 +4,10 @@ using Terraria;
 using Terraria.GameContent.UI.Elements;
 using Terraria.UI;
 
-namespace FullBodyAccessories.UI {
-    public class CustomUIPanel : UIPanel {
+namespace FullBodyAccessories.UI
+{
+    public class CustomUIPanel : UIPanel
+    {
         private readonly Color _defaultBackgroundColor = new Color(63, 82, 151) * 0.7f;
         private readonly Color _defaultBorderColor = Color.Black;
 
@@ -15,9 +17,11 @@ namespace FullBodyAccessories.UI {
 
         public bool CanDrag { get; set; } = true;
 
-        public bool Visible {
+        public bool Visible
+        {
             get => _visible;
-            set {
+            set
+            {
                 _visible = value;
 
                 BackgroundColor = _visible ? _defaultBackgroundColor : Color.Transparent;
@@ -25,57 +29,67 @@ namespace FullBodyAccessories.UI {
             }
         }
 
-        public override void MouseDown(UIMouseEvent evt) {
-            if(!_visible) return;
+        public override void MouseDown(UIMouseEvent evt)
+        {
+            if (!_visible) return;
 
             base.MouseDown(evt);
 
-            if(!CanDrag) return;
+            if (!CanDrag) return;
 
-            if(evt.MousePosition.Y >= Top.Pixels && evt.MousePosition.Y <= (Top.Pixels + PaddingTop)) {
+            if (ContainsPoint(evt.MousePosition) &&
+               !GetInnerDimensions().ToRectangle().Contains(evt.MousePosition.ToPoint()))
+            {
                 DragBegin(evt);
             }
         }
 
-        public override void MouseUp(UIMouseEvent evt) {
-            if(!_visible) return;
+        public override void MouseUp(UIMouseEvent evt)
+        {
+            if (!_visible) return;
 
             base.MouseUp(evt);
 
-            if(!CanDrag) return;
+            if (!CanDrag) return;
 
-            if(_dragging)
+            if (_dragging)
                 DragEnd(evt);
         }
 
-        public override void Update(GameTime gameTime) {
+        public override void Update(GameTime gameTime)
+        {
             base.Update(gameTime);
 
-            if(!_visible) return;
+            if (!_visible) return;
 
-            if(ContainsPoint(Main.MouseScreen)) {
+            if (ContainsPoint(Main.MouseScreen))
+            {
                 Main.LocalPlayer.mouseInterface = true;
             }
 
-            if(_dragging) {
+            if (_dragging)
+            {
                 Left.Set(Main.mouseX - _offset.X, 0);
                 Top.Set(Main.mouseY - _offset.Y, 0);
             }
 
             Rectangle parentDimensions = Parent.GetDimensions().ToRectangle();
 
-            if(!GetDimensions().ToRectangle().Intersects(parentDimensions)) {
+            if (!GetDimensions().ToRectangle().Intersects(parentDimensions))
+            {
                 Left.Pixels = Utils.Clamp(Left.Pixels, 0, parentDimensions.Right - Width.Pixels);
                 Top.Pixels = Utils.Clamp(Top.Pixels, 0, parentDimensions.Bottom - Height.Pixels);
             }
         }
 
-        private void DragBegin(UIMouseEvent e) {
+        private void DragBegin(UIMouseEvent e)
+        {
             _offset = new Vector2(e.MousePosition.X - Left.Pixels, e.MousePosition.Y - Top.Pixels);
             _dragging = true;
         }
 
-        private void DragEnd(UIMouseEvent e) {
+        private void DragEnd(UIMouseEvent e)
+        {
             Vector2 end = e.MousePosition;
             _dragging = false;
 
